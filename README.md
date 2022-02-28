e380660a86d838627ffe9b0fe109ab2b0c3190e7
Stephen Melnicki <smelnicki@smelnicki.org>
2022-02-28 11:27:35 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# stephenmelnicki.com
+
+[![License](http://img.shields.io/:license-mit-blue.svg?style=flat-square)](LICENSE)
+
+Source code behind my personal website.
---
+++ server.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const http = require('http');
+
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, world!');
+});
+
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
