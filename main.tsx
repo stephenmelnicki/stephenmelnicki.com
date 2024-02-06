fa382c1c46549d50e080c8bd673095befd57e162
Stephen Melnicki <stephen@stephenmelnicki.com>
2024-02-06 12:16:44 -0800

    initial commit

---
+++ main.tsx
@@ -0,0 +1,18 @@
+/** @jsx h */
+
+import blog from "blog";
+
+blog({
+  author: "Stephen Melnicki",
+  title: "Stephen Melnicki",
+  description: "Personal Website",
+  avatar: "/avatar.png",
+  avatarClass: "rounded-full",
+  links: [
+    { title: "Email", url: "mailto:stephen@stephenmelnicki.com" },
+    { title: "GitHub", url: "https://github.com/stephenmelnicki" },
+    { title: "LinkedIn", url: "https://www.linkedin.com/in/stephen-melnicki" },
+  ],
+  lang: "en",
+  favicon: "favicon.ico",
+});
